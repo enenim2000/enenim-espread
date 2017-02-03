d27e5d5d24d08f024affa185eb3b57e0445fa796
enenim2000 <enenim2000@gmail.com>
2017-02-03 11:21:22 +0100

    Update DBConnector.php

--- DBConnector.php
+++ DBConnector.php
@@ -3,18 +3,10 @@ class DBConnector{
     public static function getConnector(){
 
 
-        $servername = "localhost"; //$servername = "bassey.dqdemos.com";
-        $username = "root"; //$username = "dqdemosc_bassey";
-        $password = ""; //$password = "Bassey*123#";
-        $dbname = "dqdemosc_bassey";
-
-
-        /*
-        $servername = "localhost"; //$servername = "bassey.dqdemos.com";
-        $username = "dqdemosc_bassey"; //$username = "dqdemosc_bassey";
-        $password = "Bassey*123#"; //$password = "Bassey*123#";
-        $dbname = "dqdemosc_bassey";
-        */
+        $servername = "localhost"; 
+        $username = "root"; 
+        $password = "password";
+        $dbname = "db_test";
 
         // Create connection
         $conn = mysqli_connect($servername, $username, $password, $dbname);
